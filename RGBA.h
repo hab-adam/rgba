f473b00340bfb0e32a602da5e3de330f2d365c53
adam <adamhabachirov@gmail.com>
2021-07-05 22:40:17 +0300

    added const

--- RGBA.h
+++ RGBA.h
@@ -4,10 +4,10 @@ using namespace std;
 
 class RGBA{
 private:
-	uint8_t m_red;
-	uint8_t m_green;
-	uint8_t m_blue;
-	uint8_t m_alpha;
+	const uint8_t m_red;
+	const uint8_t m_green;
+	const uint8_t m_blue;
+	const uint8_t m_alpha;
 public:
 	RGBA(uint8_t red = 0, uint8_t green = 0, uint8_t blue = 0, uint8_t alpha = 255);
 	void print();
