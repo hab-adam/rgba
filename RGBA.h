4fc1736aebc576e3de99a248daec0ce2c677cfc8
adam <adamhabachirov@gmail.com>
2021-07-05 22:36:37 +0300

    debuged constructor

--- RGBA.h
+++ RGBA.h
@@ -9,7 +9,6 @@ private:
 	uint8_t m_blue;
 	uint8_t m_alpha;
 public:
-	RGBA();
 	RGBA(uint8_t red = 0, uint8_t green = 0, uint8_t blue = 0, uint8_t alpha = 255);
 	void print();
 };
--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main()
 {
+RGBA white(0);
+RGBA blue(0,0,255);
 RGBA green(0, 255, 0, 255);
 RGBA red(255, 0, 0, 255);
+white.print();
+blue.print();
 green.print();
 red.print();
 return 0;
