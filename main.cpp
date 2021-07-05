525721b3005930174b1c431e2ea0f0712c6603e3
adam <adamhabachirov@gmail.com>
2021-07-05 22:49:47 +0300

    changed main

--- main.cpp
+++ main.cpp
@@ -4,13 +4,8 @@ using namespace std;
 
 int main()
 {
-RGBA white(0);
-RGBA blue(0,0,255);
-RGBA green(0, 255, 0, 255);
-RGBA red(255, 0, 0, 255);
+RGBA white(0,135,135);
+
 white.print();
-blue.print();
-green.print();
-red.print();
 return 0;
 }
